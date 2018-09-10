cb8b643be0a213593bb9ad9fc4023ecb41405748
Adam Thomas <adam@teselagen.com>
2018-09-10 12:28:30 -0400

    adjusting to try to get static assest to serve correctly

--- server/src/index.ts
+++ server/src/index.ts
@@ -94,7 +94,7 @@ async function bootstrap() {
             } else {
                 console.warn(`No index.html found in root static path`);
             }
-            app.use(express.static(rootStaticPath));
+            app.use("/", express.static(rootStaticPath));
         }
         return new Promise((resolve, reject) => {
             try {
